227bf324a731a064d59bfb432d1eb76c7aa9b784
Gabriel Milioli <gmilioli@outlook.com>
2018-07-20 17:50:15 -0300

    Primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
+import { RestProvider } from '../providers/rest/rest';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
@@ -13,6 +16,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
+    HttpModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
@@ -24,7 +28,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthServiceProvider,
+    RestProvider
   ]
 })
 export class AppModule {}
---
+++ src/providers/auth-service/auth-service.ts
@@ -0,0 +1,123 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the AuthServiceProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class AuthServiceProvider {
+  
+  // Change to this http://ed43bb3b.ngrok.io/api/login
+  static readonly LOGIN_URL = 'http://contoh.dev/api/login';
+  // Change to this http://ed43bb3b.ngrok.io/api/register
+  static readonly REGISTER_URL = 'http://contoh.dev/api/register';
+
+  resturl : string = 'https://4p.pelainternet.com.br/rest.php';
+
+  access: boolean;
+  token: string;
+  get: any;
+  class: string = 'TalkingBus';
+
+  constructor(public http: Http) {
+  	console.log(this.http);
+/*
+  	this.http.get(this.resturl).map(res => res.json())
+        .subscribe( data => {
+          console.log(data);
+        });
+*/
+  }
+
+  // Login
+  public login(credentials) {
+    if (credentials.email === null || credentials.password === null) {
+      return Observable.throw("Please insert credentials.");
+    } else {
+      return Observable.create(observer => {
+
+        this.http.post(AuthServiceProvider.LOGIN_URL, credentials)
+        .map(res => res.json())
+        .subscribe( data => {
+          if (data.access_token) {
+            this.token = 'Bearer ' + data.access_token;
+            this.access = true;
+          } else {
+            this.access = false;
+          }
+        });
+
+        setTimeout(() => {
+              observer.next(this.access);
+          }, 500);
+
+        setTimeout(() => {
+              observer.complete();
+          }, 1000);
+
+
+      }, err => console.error(err));
+    }
+  }
+
+  // Register
+  public register(credentials) {
+    if (credentials.name === null || credentials.email === null || credentials.password === null) {
+      return Observable.throw("Please insert credentials");
+    } else {
+
+    //?class=TalkingBus&method=register
+    object: object = JSON.stringify(credentials);
+    console.log(object);
+    /*
+    object.class = this.class;
+    object.method = 'register';
+    credentials = JSON.stringify(object);
+
+	return Observable.create(observer => {
+
+        this.http.post(this.resturl, credentials)
+        .map(res => res.json())
+        .subscribe( data => {
+          console.log(data);
+        });
+
+        observer.next(true);
+        observer.complete();
+      });
+*/
+    /*
+      return Observable.create(observer => {
+
+        this.http.post(AuthServiceProvider.REGISTER_URL, credentials)
+        .map(res => res.json())
+        .subscribe( data => {
+          console.log(data);
+        });
+
+        observer.next(true);
+        observer.complete();
+      });
+      */
+    }
+  }
+
+  // Get Token
+  public getToken() {
+    return this.token;
+  }
+
+  // Logout
+  public logout() {
+    return Observable.create(observer => {
+      observer.next(true);
+      observer.complete();
+    });
+  }
+
+}
